7eac87e267d6e2cef846bfc61eff00e22664f601
Ismael Bernad Tello <bernad.teism20@zaragoza.salesianos.edu>
2024-01-11 10:46:23 +0100

    Clase 11/01/2024

---
+++ Proyectos.NET/MVC2024/MVC2024/Models/SerieModelo.cs
@@ -0,0 +1,8 @@
+﻿namespace MVC2024.Models
+{
+    public class SerieModelo
+    {
+
+
+    }
+}
---
+++ Proyectos.NET/MVC2024/MVC2024/Models/VehiculoModelo.cs
@@ -0,0 +1,16 @@
+﻿using MVC2024.Controllers;
+
+namespace MVC2024.Models
+{
+    public class VehiculoModelo
+    {
+
+        public int ID { get; set; }
+        public string? Matricula { get; set; }
+
+        public string Color { get; set; }
+
+        public SerieModelo Serie { get; set; }
+
+    }
+}
